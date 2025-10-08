718e3778d6b0e96b5730bcedce8cf3032e346f09
Pieter Luypaert <pieter.luypaert@telenet.be>
2025-10-08 20:54:22 +0200

    feat: add news component to display the latest news in a window

---
+++ news-application/src/programs/news.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Window } from "../components/window";
+
+export const news = () => {
+  return (
+    <Window
+      title="The latest news"
+      trigger={
+        <div className="program">
+          <img
+            className="icon"
+            alt="news icon"
+            src="/assets/apps/newspaper.png"
+          />
+          <span className="name">News</span>
+        </div>
+      }
+
+    >
+    </Window>
+  );
+};
