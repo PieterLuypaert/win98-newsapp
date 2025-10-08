927796cb47cc6cb0c6fa469e164a2c789067a793
Pieter Luypaert <pieter.luypaert@telenet.be>
2025-10-08 20:54:09 +0200

    feat: add FullscreenButton, Separator, and StartButton components for taskbar functionality

---
+++ news-application/src/components/taskbar/FullscreenButton.jsx
@@ -0,0 +1,14 @@
+import { Button } from "../ui/Button";
+
+export const FullscreenButton = ({ onClick }) => {
+  return (
+    <Button
+      className="win98-button"
+      title="Fullscreen"
+      onClick={onClick}
+      variant="win98"
+    >
+      ⛶
+    </Button>
+  );
+};
---
+++ news-application/src/components/taskbar/StartButton.jsx
@@ -0,0 +1,13 @@
+export const StartButton = () => {
+  return (
+
+    <button className="start" title="Start">
+      <img
+        className="icon"
+        src="/assets/windows.png"
+        alt="windows 98 start logo"
+      />
+      <span className="label">Start</span>
+    </button>
+  );
+};
