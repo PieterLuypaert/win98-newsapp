8229cce8a44ee8d15b807c743be43c7339070705
Pieter Luypaert <pieter.luypaert@telenet.be>
2025-10-08 20:53:28 +0200

    feat: create Desktop component with ProgramGrid integration

---
+++ news-application/src/components/Desktop.jsx
@@ -0,0 +1,10 @@
+import "../styles/layout/desktop.css";
+import { ProgramGrid } from "./desktop/ProgramGrid";
+
+export const Desktop = () => {
+  return (
+    <div className="desktop">
+      <ProgramGrid />
+    </div>
+  );
+};
