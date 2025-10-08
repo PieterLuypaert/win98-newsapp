8d06b1961faaa437f66ec21cb44997e2a4b1c553
Pieter Luypaert <pieter.luypaert@telenet.be>
2025-10-08 20:54:17 +0200

    feat: add Button component for reusable button functionality

---
+++ news-application/src/components/ui/Button.jsx
@@ -0,0 +1,21 @@
+export const Button = ({
+  children,
+  onClick,
+  className = "",
+  autoFocus = false,
+  variant = "default",
+  ...props
+}) => {
+  const baseClass = variant === "win98" ? "win98-button" : "button";
+
+  return (
+    <button
+      className={`${baseClass} ${className}`}
+      onClick={onClick}
+      autoFocus={autoFocus}
+      {...props}
+    >
+      {children}
+    </button>
+  );
+};
