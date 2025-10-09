a094c7b870d66b58563ee74a01e397a454f77c95
Pieter Luypaert <pieter.luypaert@telenet.be>
2025-10-09 15:07:25 +0200

    feat: replace TaskbarTabs component with TaskbarCredits and remove unused styles

--- news-application/src/components/Taskbar.jsx
+++ news-application/src/components/Taskbar.jsx
@@ -2,7 +2,7 @@ import "../styles/layout/taskbar.css";
 import { StartButton } from "./design/startButton/StartButton";
 import { Separator } from "./design/seperator/Separator";
 import { FullscreenButton } from "./design/fullscreenButton/FullscreenButton";
-import { TaskbarTabs } from "./design/taskbar/TaskbarTabs";
+import { TaskbarTabs } from "./design/taskbar/TaskbarCredits";
 import { TaskbarTime } from "./design/TaskbarTime/TaskbarTime";
 
 export const Taskbar = ({
