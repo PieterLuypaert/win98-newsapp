d037c1e45c4d0741d01b018414b382505c672c32
Pieter Luypaert <pieter.luypaert@telenet.be>
2025-10-09 21:08:37 +0200

    refactor: remove Login component from ProgramGrid for cleaner structure

--- news-application/src/components/design/ProgramGrid/ProgramGrid.jsx
+++ news-application/src/components/design/ProgramGrid/ProgramGrid.jsx
@@ -1,11 +1,9 @@
 import { news as News } from "../NewsApp/news";
-import { login as Login } from "../LoginApp/Login";
 
 export const ProgramGrid = () => {
   return (
     <div className="programs">
       <News />
-      <Login />
     </div>
   );
 };
