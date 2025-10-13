3dbd614af3f95f0c83d3cd45906be54b621e2873
Pieter Luypaert <pieter.luypaert@telenet.be>
2025-10-13 18:12:57 +0200

    refactor: consolidate App component by removing Home and integrating its functionality directly

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,45 @@
 import "./styles/index.css";
-import React from "react";
-import Home from "./pages/Home";
+import React, { useState, useEffect } from "react";
+import { Desktop } from "./components/Desktop";
+import { Taskbar } from "./components/Taskbar";
 
 function App() {
-  return <Home />;
+  const [time, setTime] = useState(new Date());
+  const [isFullscreen, setIsFullscreen] = useState(false);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+
+    return () => clearInterval(timer);
+  }, []);
+
+  const handleFullscreen = () => {
+    if (!document.fullscreenElement) {
+      document.documentElement.requestFullscreen();
+      setIsFullscreen(true);
+    } else {
+      document.exitFullscreen();
+      setIsFullscreen(false);
+    }
+  };
+
+  const formattedTime = time.toLocaleTimeString("nl-BE", {
+    hour: "2-digit",
+    minute: "2-digit",
+  });
+
+  return (
+    <div className="app">
+      <Desktop />
+      <Taskbar
+        time={formattedTime}
+        showFullscreenButton={true}
+        onFullscreen={handleFullscreen}
+      />
+    </div>
+  );
 }
 
 export default App;
