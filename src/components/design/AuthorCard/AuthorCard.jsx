ca18994fe7843466ba0edd61747a6ba5d7f059d2
Pieter Luypaert <pieter.luypaert@telenet.be>
2025-10-15 15:54:45 +0200

    feat: implement AuthorCard component with styling and social links

---
+++ src/components/design/AuthorCard/AuthorCard.jsx
@@ -0,0 +1,46 @@
+import "./AuthorCard.css";
+
+export const AuthorCard = ({ author }) => {
+  if (!author) return null;
+
+  return (
+    <div className="author-card">
+      <div className="author-card-header">About the Author</div>
+      <div className="author-card-content">
+        <img
+          src={author.avatar}
+          alt={`${author.firstName} ${author.lastName}`}
+          className="author-avatar"
+        />
+        <h3 className="author-name">
+          {author.firstName} {author.lastName}
+        </h3>
+        <p className="author-bio">{author.bio}</p>
+        {author.social && (
+          <div className="author-social">
+            {author.social.twitter && (
+              <a
+                href={author.social.twitter}
+                className="author-social-link"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Twitter
+              </a>
+            )}
+            {author.social.linkedin && (
+              <a
+                href={author.social.linkedin}
+                className="author-social-link"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                LinkedIn
+              </a>
+            )}
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
