9bd28f54c43c3c4720b460757b663115c93ed694
Pieter Luypaert <pieter.luypaert@telenet.be>
2025-10-16 13:52:33 +0200

    feat: add Bookmark component with styles for taskbar integration

---
+++ src/components/design/Bookmarks/Bookmarks.jsx
@@ -0,0 +1,13 @@
+export const Bookmark = () => {
+  return (
+
+    <button className="Bookmark" title="Bookmarks">
+      <img
+        className="icon"
+        src="/assets/apps/bookmarks.png"
+        alt="windows 98 Bookmarks logo"
+      />
+      <span className="label">Bookmarks</span>
+    </button>
+  );
+};
