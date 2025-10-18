f10e5658ae58af3ff6f23c8b67ab5ec0e14e164b
Pieter Luypaert <pieter.luypaert@telenet.be>
2025-10-18 15:56:35 +0200

    feat: add Button component stories for design showcase

---
+++ src/components/design/Button/Button.stories.jsx
@@ -0,0 +1,29 @@
+import "../../../styles/index.css";
+import React from "react";
+import { Button } from "./Button";
+
+export default {
+  title: "Design/Button",
+  component: Button,
+  argTypes: {
+    children: { control: "text", name: "Label" },
+    variant: { control: { type: "select", options: ["default", "win98"] } },
+    className: { control: "text" },
+    autoFocus: { control: "boolean" },
+    onClick: { action: "onClick" },
+  },
+};
+
+const Template = (args) => (
+  <div style={{ padding: 16 }}>
+    <Button {...args}>{args.children}</Button>
+  </div>
+);
+
+export const Default = Template.bind({});
+Default.args = {
+  children: "Click me",
+  variant: "win98",
+  className: "",
+  autoFocus: false,
+};
