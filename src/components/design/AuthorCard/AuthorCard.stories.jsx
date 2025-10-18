b67afd669625e6b2df712fd184a6c686deee5d3e
Pieter Luypaert <pieter.luypaert@telenet.be>
2025-10-18 12:47:15 +0200

    feat: add AuthorCard component stories for design showcase

---
+++ src/components/design/AuthorCard/AuthorCard.stories.jsx
@@ -0,0 +1,55 @@
+import "../../../styles/index.css"; // laad globale styles zodat AuthorCard styling zichtbaar is
+import React from "react";
+import { AuthorCard } from "./AuthorCard";
+
+export default {
+  title: "Design/AuthorCard",
+  component: AuthorCard,
+  argTypes: {
+    firstName: { control: "text" },
+    lastName: { control: "text" },
+    bio: { control: "text" },
+    avatar: { control: "text" },
+    twitter: { control: "text" },
+    linkedin: { control: "text" },
+  },
+};
+
+const Template = (args) => {
+  const author = {
+    firstName: args.firstName,
+    lastName: args.lastName,
+    bio: args.bio,
+    avatar: args.avatar,
+    social: {
+      twitter: args.twitter || "",
+      linkedin: args.linkedin || "",
+    },
+  };
+
+  return (
+    <div style={{ padding: 16, maxWidth: 360 }}>
+      <AuthorCard author={author} />
+    </div>
+  );
+};
+
+export const Default = Template.bind({});
+Default.args = {
+  firstName: "Pieter",
+  lastName: "Luypaert",
+  bio: "Frontend developer and Windows nostalgia enthusiast. Loves building retro-inspired UIs and readable news apps.",
+  avatar: "/assets/avatar-default.png",
+  twitter: "https://twitter.com/example",
+  linkedin: "https://www.linkedin.com/in/example",
+};
+
+export const NoSocial = Template.bind({});
+NoSocial.args = {
+  firstName: "Alex",
+  lastName: "Example",
+  bio: "Guest author without social links.",
+  avatar: "/assets/avatar-default.png",
+  twitter: "",
+  linkedin: "",
+};
