c21bebbe9bee4b44eaa08d2706311be13d2b3b69
Pieter Luypaert <pieter.luypaert@telenet.be>
2025-10-18 12:40:37 +0200

    feat: add BookmarkButton component stories for design showcase

---
+++ src/components/design/BookmarkButton/BookmarkButton.stories.jsx
@@ -0,0 +1,27 @@
+import "../../../styles/index.css"; 
+import React from "react";
+import { BookmarkButton } from "./BookmarkButton";
+
+export default {
+  title: "Design/BookmarkButton",
+  component: BookmarkButton,
+  argTypes: {
+    isBookmarked: { control: "boolean" },
+  },
+};
+
+const Template = (args) => (
+  <div style={{ width: 240 }}>
+    <BookmarkButton {...args} />
+  </div>
+);
+
+export const Default = Template.bind({});
+Default.args = {
+  isBookmarked: false,
+};
+
+export const Bookmarked = Template.bind({});
+Bookmarked.args = {
+  isBookmarked: true,
+};
