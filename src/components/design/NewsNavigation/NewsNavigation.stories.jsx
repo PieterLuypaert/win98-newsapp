ce7df6096b0fef78fc2da1e99602cad6a4f2677e
Pieter Luypaert <pieter.luypaert@telenet.be>
2025-10-18 16:26:52 +0200

    feat: add NewsNavigation component stories for design showcase

---
+++ src/components/design/NewsNavigation/NewsNavigation.stories.jsx
@@ -0,0 +1,46 @@
+import "../../../styles/index.css";
+import React from "react";
+import { NewsNavigation } from "./NewsNavigation";
+import categoriesData from "../../../data/categories.json";
+
+export default {
+  title: "Design/NewsNavigation",
+  component: NewsNavigation,
+  argTypes: {
+    activeCategory: {
+      control: { type: "select" },
+      options: ["", ...categoriesData.map((c) => c.slug)],
+    },
+    showSearch: { control: "boolean" },
+    onCategoryClick: { action: "onCategoryClick" },
+    onSearch: { action: "onSearch" },
+  },
+};
+
+const Template = (args) => (
+  <div style={{ padding: 16, maxWidth: 900 }}>
+    <NewsNavigation
+      activeCategory={args.activeCategory || null}
+      onCategoryClick={args.onCategoryClick}
+      onSearch={args.showSearch ? args.onSearch : undefined}
+    />
+  </div>
+);
+
+export const Default = Template.bind({});
+Default.args = {
+  activeCategory: "",
+  showSearch: true,
+};
+
+export const CategoryActive = Template.bind({});
+CategoryActive.args = {
+  activeCategory: categoriesData[0]?.slug || "",
+  showSearch: true,
+};
+
+export const WithoutSearch = Template.bind({});
+WithoutSearch.args = {
+  activeCategory: "",
+  showSearch: false,
+};
