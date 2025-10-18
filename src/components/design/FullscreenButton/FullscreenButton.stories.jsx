99a2bbcd92df9ce3212269c706815b57cdd1eb53
Pieter Luypaert <pieter.luypaert@telenet.be>
2025-10-18 13:03:35 +0200

    feat: add FullscreenButton component stories for design showcase

---
+++ src/components/design/FullscreenButton/FullscreenButton.stories.jsx
@@ -0,0 +1,22 @@
+import "../../../styles/index.css";
+import React from "react";
+import { FullscreenButton } from "./FullscreenButton";
+
+export default {
+  title: "Design/FullscreenButton",
+  component: FullscreenButton,
+  argTypes: {
+    onClick: { action: "onClick" },
+  },
+};
+
+const Template = (args) => (
+  <div
+    style={{ padding: 16, display: "inline-block", background: "transparent" }}
+  >
+    <FullscreenButton {...args} />
+  </div>
+);
+
+export const Default = Template.bind({});
+Default.args = {};
