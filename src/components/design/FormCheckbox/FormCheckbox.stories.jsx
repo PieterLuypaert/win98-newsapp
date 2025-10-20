d634f34c2b6fdca0f8201ecb61c645da47b6f746
Pieter Luypaert <pieter.luypaert@telenet.be>
2025-10-20 22:37:49 +0200

    feat: add FormCheckbox story for design documentation

---
+++ src/components/design/FormCheckbox/FormCheckbox.stories.jsx
@@ -0,0 +1,26 @@
+import "../../../styles/index.css";
+import React from "react";
+import { FormCheckbox } from "./FormCheckbox";
+
+export default {
+  title: "Design/FormCheckbox",
+  component: FormCheckbox,
+  argTypes: {
+    label: { control: "text" },
+    required: { control: "boolean" },
+    className: { control: "text" },
+  },
+};
+
+const Template = (args) => (
+  <div style={{ padding: 16, maxWidth: 420 }}>
+    <FormCheckbox {...args} />
+  </div>
+);
+
+export const Default = Template.bind({});
+Default.args = {
+  label: "Akkoord met voorwaarden",
+  required: true,
+  className: "",
+};
