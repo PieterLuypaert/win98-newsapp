1ea00b25b760bac0ca8397c920cb123d3139d68b
Pieter Luypaert <pieter.luypaert@telenet.be>
2025-10-23 20:15:09 +0200

    feat: add QueryClientProvider to Storybook preview for improved query handling

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,13 +1,28 @@
+import React from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
 /** @type { import('@storybook/react-vite').Preview } */
 const preview = {
   parameters: {
     controls: {
       matchers: {
-       color: /(background|color)$/i,
-       date: /Date$/i,
+        color: /(background|color)$/i,
+        date: /Date$/i,
       },
     },
   },
 };
+
+const queryClient = new QueryClient({
+  defaultOptions: { queries: { retry: false } },
+});
+
+export const decorators = [
+  (Story) => (
+    <QueryClientProvider client={queryClient}>
+      <Story />
+    </QueryClientProvider>
+  ),
+];
 
 export default preview;
