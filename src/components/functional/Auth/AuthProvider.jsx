4946dba2ba534854dbd2016655e76316525bb52d
Pieter Luypaert <pieter.luypaert@telenet.be>
2025-10-23 20:38:08 +0200

    fix: correct import path for Storage module in AuthProvider

--- src/components/functional/Auth/AuthProvider.jsx
+++ src/components/functional/Auth/AuthProvider.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { AuthContext } from "./AuthContext";
-import * as Storage from "@core/storage";
+import * as Storage from "../../../core/storage";
 
 const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(Storage.getUser());
