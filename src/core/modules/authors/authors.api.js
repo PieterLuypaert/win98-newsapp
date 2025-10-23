12d0554837ed9839ee35cb9e5ed1f36d22e388cc
Pieter Luypaert <pieter.luypaert@telenet.be>
2025-10-23 19:52:49 +0200

    feat: implement API functions for fetching authors and author details

---
+++ src/core/modules/authors/authors.api.js
@@ -0,0 +1,9 @@
+import API from "../../networking/api";
+
+export const fetchAuthors = () => {
+    return API.get("/news/authors.json").then(({ data }) => data);
+};
+
+export const fetchAuthorItem = (id) => {
+    return API.get(`/news/authors/${id}.json`).then(({ data }) => data);
+};
