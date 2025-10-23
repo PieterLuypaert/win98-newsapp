c5de71b85e2a0d5cb85dd3c061df711f40c8049b
Pieter Luypaert <pieter.luypaert@telenet.be>
2025-10-23 19:10:38 +0200

    refactor: reorganize imports and clean up main application structure

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router";
 import App from "./App";
-
 import Home from "./pages/Home";
 import News from "./pages/News";
 import Bookmarks from "./pages/Bookmarks";
@@ -10,7 +9,7 @@ import Category from "./pages/Category";
 import Article from "./pages/Article";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
-import { QueryClientProvider } from "@tanstack/react-query";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const root = document.getElementById("root");
 
@@ -19,18 +18,18 @@ const client = new QueryClient();
 ReactDOM.createRoot(root).render(
   <React.StrictMode>
     <QueryClientProvider client={client}>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />}>
-          <Route path="news" element={<News />} />
-          <Route path="bookmarks" element={<Bookmarks />} />
-          <Route path="category/:categorySlug" element={<Category />} />
-          <Route path="article/:articleSlug" element={<Article />} />
-          <Route path="login" element={<Login />} />
-          <Route path="register" element={<Register />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />}>
+            <Route path="news" element={<News />} />
+            <Route path="bookmarks" element={<Bookmarks />} />
+            <Route path="category/:categorySlug" element={<Category />} />
+            <Route path="article/:articleSlug" element={<Article />} />
+            <Route path="login" element={<Login />} />
+            <Route path="register" element={<Register />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </QueryClientProvider>
   </React.StrictMode>
 );
