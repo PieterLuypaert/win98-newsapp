fc3e86497a96a79263cf81b63b6bb942d1e4ed5e
Pieter Luypaert <pieter.luypaert@telenet.be>
2025-10-24 17:27:33 +0200

    feat: comment out response delay interceptor in API configuration

--- src/core/networking/api.js
+++ src/core/networking/api.js
@@ -4,4 +4,14 @@ const API = axios.create({
   baseURL: import.meta.env.VITE_BASE_API_URL,
 });
 
+// // 2 seconden vertraging voor elke response
+// const delay = (ms) => new Promise((r) => setTimeout(r, ms));
+
+// API.interceptors.response.use(
+//   async (response) => {
+//     await delay(2000);
+//     return response;
+//   },
+// );
+
 export default API;
