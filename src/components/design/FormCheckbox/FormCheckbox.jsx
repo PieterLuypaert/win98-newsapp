4a7a23364cedbd742aed8bf41721f311bee044e8
Pieter Luypaert <pieter.luypaert@telenet.be>
2025-10-24 22:14:39 +0200

    feat: add checked and onChange props to FormCheckbox component for enhanced interactivity

--- src/components/design/FormCheckbox/FormCheckbox.jsx
+++ src/components/design/FormCheckbox/FormCheckbox.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 import "./FormCheckbox.css";
 
 export const FormCheckbox = ({
@@ -7,6 +8,9 @@ export const FormCheckbox = ({
   label = "",
   required = false,
   className = "",
+  checked,
+  onChange,
+  ...rest
 }) => {
   const inputId = id || `${name}-id`;
 
@@ -19,11 +23,24 @@ export const FormCheckbox = ({
           type="checkbox"
           className={`checkbox-input ${className}`.trim()}
           required={required}
+          checked={!!checked}
+          onChange={onChange}
+          {...rest}
         />{" "}
         {label}
       </label>
     </div>
   );
+};
+
+FormCheckbox.propTypes = {
+  name: PropTypes.string,
+  id: PropTypes.string,
+  label: PropTypes.string,
+  required: PropTypes.bool,
+  className: PropTypes.string,
+  checked: PropTypes.bool,
+  onChange: PropTypes.func,
 };
 
 export default FormCheckbox;
