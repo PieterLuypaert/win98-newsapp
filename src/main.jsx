10af9f802eefdf8b18f83a4fb232720da733ead3
Pieter Luypaert <pieter.luypaert@telenet.be>
2025-10-24 22:09:56 +0200

    refactor: restructure component hierarchy for improved context management

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router";
+import { BrowserRouter, Routes, Route } from "react-router";
 import App from "./App";
 import Home from "./pages/Home";
 import News from "./pages/News";
@@ -10,26 +10,31 @@ import Article from "./pages/Article";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import AuthProvider from "./components/functional/Auth/AuthProvider";
+import BookmarksProvider from "./components/functional/Bookmarks/BookmarksProvider";
 
 const root = document.getElementById("root");
-
 const client = new QueryClient();
 
 ReactDOM.createRoot(root).render(
   <React.StrictMode>
     <QueryClientProvider client={client}>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<App />}>
-            <Route path="news" element={<News />} />
-            <Route path="bookmarks" element={<Bookmarks />} />
-            <Route path="category/:categorySlug" element={<Category />} />
-            <Route path="article/:articleSlug" element={<Article />} />
-            <Route path="login" element={<Login />} />
-            <Route path="register" element={<Register />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
+      <AuthProvider>
+        <BookmarksProvider>
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<App />}>
+                <Route path="news" element={<News />} />
+                <Route path="bookmarks" element={<Bookmarks />} />
+                <Route path="category/:categorySlug" element={<Category />} />
+                <Route path="article/:articleSlug" element={<Article />} />
+                <Route path="login" element={<Login />} />
+                <Route path="register" element={<Register />} />
+              </Route>
+            </Routes>
+          </BrowserRouter>
+        </BookmarksProvider>
+      </AuthProvider>
     </QueryClientProvider>
   </React.StrictMode>
 );
