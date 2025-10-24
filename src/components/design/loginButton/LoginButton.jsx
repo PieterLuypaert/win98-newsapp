29ba582524e06eb31f391d90cd24da627c853d1b
Pieter Luypaert <pieter.luypaert@telenet.be>
2025-10-24 22:12:19 +0200

    feat: enhance LoginButton to include authentication logic for dynamic login/logout

--- src/components/design/loginButton/LoginButton.jsx
+++ src/components/design/loginButton/LoginButton.jsx
@@ -1,7 +1,22 @@
 import { useNavigate } from "react-router";
+import useAuth from "../../functional/Auth/UseAuth";
 
 export const LoginButton = () => {
   const navigate = useNavigate();
+  const auth = useAuth();
+
+  if (auth?.isAuthenticated) {
+    return (
+      <button
+        className="TaskbarButton"
+        title="Logout"
+        onClick={() => auth.logout()}
+      >
+        <img className="icon" src="/assets/apps/login.png" alt="logout icon" />
+        <span className="label">Logout</span>
+      </button>
+    );
+  }
 
   return (
     <button
