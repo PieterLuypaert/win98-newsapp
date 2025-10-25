16f2b43937207720cd38167af1b3705a44f932c0
Pieter Luypaert <pieter.luypaert@telenet.be>
2025-10-25 11:43:52 +0200

    feat: update ArticleSidebar to use a customizable BookmarkComponent for improved flexibility

--- src/components/design/ArticleSidebar/ArticleSidebar.jsx
+++ src/components/design/ArticleSidebar/ArticleSidebar.jsx
@@ -1,15 +1,21 @@
 import React from "react";
 import "./ArticleSidebar.css";
 import { AuthorCard } from "../AuthorCard/AuthorCard";
-import { BookmarkButton } from "../BookmarkButton/BookmarkButton";
 import { ProgressBar } from "../ProgressBar/ProgressBar";
 
-export const ArticleSidebar = ({ author, articleId, scrollProgress }) => {
+export const ArticleSidebar = ({
+  author,
+  articleId,
+  scrollProgress,
+  BookmarkComponent = null,
+}) => {
   return (
     <aside className="article-sidebar">
       <AuthorCard author={author} />
-      <BookmarkButton articleId={articleId} />
+      {BookmarkComponent ? <BookmarkComponent articleId={articleId} /> : null}
       <ProgressBar progress={scrollProgress} />
     </aside>
   );
 };
+
+export default ArticleSidebar;
