0df3073675f3faadb25d72d578cdcd9732118fd4
Pieter Luypaert <pieter.luypaert@telenet.be>
2025-10-25 12:01:07 +0200

    feat: reduce response delay from 2 seconds to 1 second

--- src/core/networking/api.js
+++ src/core/networking/api.js
@@ -4,11 +4,11 @@ const API = axios.create({
   baseURL: import.meta.env.VITE_BASE_API_URL,
 });
 
-// // 2 seconden vertraging voor elke response
+// // 1 seconden vertraging voor elke response
 const delay = (ms) => new Promise((r) => setTimeout(r, ms));
 
 API.interceptors.response.use(async (response) => {
-  await delay(2000);
+  await delay(1000);
   return response;
 });
 
