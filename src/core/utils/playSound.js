ae7786e4b6e6b0f0166c9abe6d442cf8e84cefdf
Pieter Luypaert <pieter.luypaert@telenet.be>
2025-11-03 22:49:24 +0100

    refactor: remove silent audio playback for unlocking audio

--- src/core/utils/playSound.js
+++ src/core/utils/playSound.js
@@ -3,12 +3,6 @@ let audioUnlocked = false;
 export const unlockAudio = () => {
   if (audioUnlocked) return Promise.resolve();
 
-  // Probeer een stil geluid af te spelen om audio te unlocken
-  const silentAudio = new Audio();
-  silentAudio.volume = 0.01;
-  silentAudio.src =
-    "data:audio/wav;base64,UklGRiQAAABXQVZFZm10IBAAAAABAAEAQB8AAEAfAAABAAgAAABmYWN0BAAAAAAAAABkYXRhAAAAAA==";
-
   return silentAudio
     .play()
     .then(() => {
