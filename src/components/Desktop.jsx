c5ececac77749afd61af5c1628135a2f5290a96f
Pieter Luypaert <pieter.luypaert@telenet.be>
2025-11-03 21:21:39 +0100

    feat: refactor Desktop component to manage icon positions and improve rendering

--- src/components/Desktop.jsx
+++ src/components/Desktop.jsx
@@ -1,21 +1,51 @@
+import { useState } from "react";
 import { useNavigate } from "react-router";
 import "../styles/layout/programs.css";
-import { DesktopIcon } from "./design/DesktopIcon/DesktopIcon";
+import { DesktopIconContainer } from "./functional/DesktopIcon/DesktopIconContainer";
 import { Clippy } from "./design/Clippy/Clippy";
+import * as Storage from "../core/storage";
 
 export const Desktop = ({ showIcons = true }) => {
   const navigate = useNavigate();
+  const [iconPositions, setIconPositions] = useState(() =>
+    Storage.getIconPositions()
+  );
+
+  const icons = [
+    {
+      id: "news",
+      icon: "/assets/apps/newspaper.png",
+      label: "News",
+      onClick: () => navigate("/news"),
+      isImage: true,
+    },
+  ];
+
+  const handlePositionChange = (iconId, newPosition) => {
+    const updatedPositions = {
+      ...iconPositions,
+      [iconId]: newPosition,
+    };
+    setIconPositions(updatedPositions);
+    Storage.saveIconPositions(updatedPositions);
+  };
 
   return (
     <div className="desktop">
       {showIcons && (
         <div className="desktop-icons">
-          <DesktopIcon
-            icon="/assets/apps/newspaper.png"
-            label="News"
-            onClick={() => navigate("/news")}
-            isImage={true}
-          />
+          {icons.map((icon) => (
+            <DesktopIconContainer
+              key={icon.id}
+              id={icon.id}
+              icon={icon.icon}
+              label={icon.label}
+              onClick={icon.onClick}
+              isImage={icon.isImage}
+              position={iconPositions[icon.id]}
+              onPositionChange={handlePositionChange}
+            />
+          ))}
         </div>
       )}
       <Clippy message="Klik op het News icoon om te beginnen." />
