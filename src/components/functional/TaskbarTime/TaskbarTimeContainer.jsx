6e9376deb70668fc2c9c48600f46cb455176699e
Pieter Luypaert <pieter.luypaert@telenet.be>
2025-11-03 21:36:37 +0100

    feat: implement TaskbarTimeContainer to manage and display current time

---
+++ src/components/functional/TaskbarTime/TaskbarTimeContainer.jsx
@@ -0,0 +1,23 @@
+import React, { useState, useEffect } from "react";
+import { TaskbarTime } from "../../design/TaskbarTime/TaskbarTime";
+
+export const TaskbarTimeContainer = () => {
+  const [time, setTime] = useState(new Date());
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+
+    return () => clearInterval(timer);
+  }, []);
+
+  const formattedTime = time.toLocaleTimeString("nl-BE", {
+    hour: "2-digit",
+    minute: "2-digit",
+  });
+
+  return <TaskbarTime time={formattedTime} />;
+};
+
+export default TaskbarTimeContainer;
