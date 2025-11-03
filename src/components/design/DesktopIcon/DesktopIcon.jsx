4021df78502f9559decef7ad6f5066320ff04ee6
Pieter Luypaert <pieter.luypaert@telenet.be>
2025-11-03 21:21:46 +0100

    feat: implement DesktopIconContainer for drag-and-drop functionality and position management

--- src/components/design/DesktopIcon/DesktopIcon.jsx
+++ src/components/design/DesktopIcon/DesktopIcon.jsx
@@ -1,17 +1,30 @@
-import React from "react";
+import React, { forwardRef } from "react";
 import "./DesktopIcon.css";
 
-export const DesktopIcon = ({ icon, label, onClick, isImage = false }) => {
-  return (
-    <div className="desktop-icon" onClick={onClick}>
-      <div className="desktop-icon-image">
-        {isImage ? (
-          <img src={icon} alt={label} className="desktop-icon-img" />
-        ) : (
-          icon
-        )}
+export const DesktopIcon = forwardRef(
+  (
+    { icon, label, isImage = false, isDragging = false, onMouseDown, onClick },
+    ref
+  ) => {
+    return (
+      <div
+        ref={ref}
+        className={`desktop-icon ${isDragging ? "dragging" : ""}`}
+        onMouseDown={onMouseDown}
+        onClick={onClick}
+        style={{ cursor: isDragging ? "grabbing" : "grab" }}
+      >
+        <div className="desktop-icon-image">
+          {isImage ? (
+            <img src={icon} alt={label} className="desktop-icon-img" />
+          ) : (
+            <span>{icon}</span>
+          )}
+        </div>
+        <span className="desktop-icon-label">{label}</span>
       </div>
-      <div className="desktop-icon-label">{label}</div>
-    </div>
-  );
-};
+    );
+  }
+);
+
+DesktopIcon.displayName = "DesktopIcon";
