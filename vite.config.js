e61686173d2a378a2ba4fcf716da84b91968b67a
Pieter Luypaert <pieter.luypaert@telenet.be>
2025-11-03 21:59:02 +0100

    refactor: add missing alias paths in Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,11 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+      "@design": path.resolve(__dirname, "./src/components/design"),
+      "@functional": path.resolve(__dirname, "./src/components/functional"),
+      "@styles": path.resolve(__dirname, "./src/styles"),
+      "@core": path.resolve(__dirname, "./src/core"),
+      "@pages": path.resolve(__dirname, "./src/pages"),
     },
   },
 });
